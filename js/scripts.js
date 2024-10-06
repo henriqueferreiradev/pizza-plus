75d23c20eee3279f6ee088412bed94ac62ab637c
Henrique Ferreira <henriquef501@gmail.com>
2024-10-06 09:19:31 -0300

    styles att

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
- 
+document.querySelector(".botao-continuar-comp").addEventListener('click', function () {
+    var carrinho = document.querySelector(".container-carrinho");
+    carrinho.classList.add("fechar");
+    setTimeout(function () {
+        carrinho.style.display = "none";
+    }, 500)
+})
+
+document.querySelector(".navbar-btn").addEventListener("click", function () {
+    var carrinho = document.querySelector(".container-carrinho");
+    carrinho.style.display = "block";
+    carrinho.classList.remove("fechar");
+})
